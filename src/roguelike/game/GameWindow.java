dc8c619c1a8515ef4a9bbf20c99d16d2cfb6a2a6
UNLUCKY <oss314y08@i.softbank.jp>
2023-09-19 03:12:36 +0900

    fix : windowをcloseする処理

--- src/roguelike/game/GameWindow.java
+++ src/roguelike/game/GameWindow.java
@@ -1,14 +1,19 @@
 package roguelike.game;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import javax.swing.JFrame;
 
 import roguelike.utils.logger.SingletonSimpleLogger;
 
-public class GameWindow extends JFrame implements Runnable {
+public class GameWindow extends JFrame implements Runnable, WindowListener {
+	boolean gameActive = true;
 
 	public GameWindow(String title, int width, int height) {
 		super(title);
-		super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		super.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		super.addWindowListener(this);
 		super.setSize(width, height);
 		super.setLocationRelativeTo(null);
 		super.setResizable(false);
@@ -19,7 +24,7 @@ public class GameWindow extends JFrame implements Runnable {
 	@Override
 	public void run() {
 		var logger = SingletonSimpleLogger.getLogger();
-		while (true) {
+		while (gameActive) {
 			try {
 				Thread.sleep(5);
 				this.repaint();
@@ -29,6 +34,49 @@ public class GameWindow extends JFrame implements Runnable {
 			}
 		}
 		//System.exit(0);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		gameActive = false;
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
 	}
 
 }
